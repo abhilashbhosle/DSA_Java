53e55c4e868bc466caa1c655fc2b3a85661c668d
Abhilash Bhosle <abhilashbhosle10@gmail.com>
2024-09-01 13:44:00 +0530

    ceiling and floor in binary search

---
+++ BinarySearch/Ceiling.java
@@ -0,0 +1,29 @@
+// It returns smallest number greater then or equall to target.
+
+public class Ceiling {
+
+    public static void main(String[] args) {
+		int[] arr={1,4,8,14,16,20,32};
+		int target=15;
+		int ans=findCeiling(arr, target);
+		System.out.println(ans);
+    }
+
+    static int findCeiling(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target > arr[mid]) {
+                start = mid + 1;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return start;
+
+    }
+}
