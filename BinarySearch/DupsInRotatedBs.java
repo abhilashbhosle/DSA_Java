b22ac78e8c3894054bcba23c024373e85d82fb27
Abhilash Bhosle <abhilashbhosle10@gmail.com>
2024-09-11 15:21:58 +0530

    Duplicates in rotated binary search

---
+++ BinarySearch/DupsInRotatedBs.java
@@ -0,0 +1,70 @@
+
+public class DupsInRotatedBs {
+
+    public static void main(String[] args) {
+        int[] nums = {1};
+        int target = 0;
+        int ans = findTarget(nums, target);
+        System.out.println(ans);
+    }
+
+    static int findTarget(int[] nums, int target) {
+        int pivot = findPivot(nums);
+		if (pivot == -1) {
+            return BiarySearch(nums, target, 0, nums.length - 1);
+        }
+        if (nums[pivot] == target) {
+            return pivot;
+        }
+        
+        if (target >= nums[0]) {
+            return BiarySearch(nums, target, 0, pivot - 1);
+        } else {
+            return BiarySearch(nums, target, pivot + 1, nums.length - 1);
+        }
+    }
+
+    static int BiarySearch(int[] arr, int target, int start, int end) {
+        System.out.println(arr);
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > target) {
+                end = mid - 1;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    static int findPivot(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid < end && nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && nums[mid] < nums[mid - 1]) {
+                return mid - 1;
+            }
+            if (nums[mid] == nums[start] && nums[mid] == nums[end]) {
+                if (start < end && nums[start] > nums[start + 1]) {
+                    return start;
+                }
+                start++;
+                if (end>start && nums[end] < nums[end - 1]) {
+                    return end - 1;
+                }
+                end--;
+            } else if ( nums[start] < nums[mid] || (nums[start] == nums[mid] && nums[mid] > nums[end])) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
--- BinarySearch/RotatedBs.java
+++ BinarySearch/RotatedBs.java
@@ -1,65 +1,68 @@
 // Rotated binary search usually based on finding pivot depending on four cases.
 // Basically to resolve the rotated bs we need to find the peak/pivot/larger number in givern array,
 // so that the left and right elements of the pivot are ascending ordered.
-public class RotatedBs{
-	public static void main(String[] args) {
-		int[] arr={4,5,6,7,0,1,2};
-		int target=0;
-		int ans=RotatedTargetSearch(arr,target);
-		System.out.println(ans);
-	}
-	static int RotatedTargetSearch(int[] arr,int target){
-		int pivot=findPivot(arr);
-		if(arr[pivot]==target){
-			return pivot;
-		}
-		if(pivot==-1){ // this means the array is not rotated just do simple bs
-		  return BiarySearch(arr, target,0,arr.length-1);
-		}
-		if(target>=arr[0]){
-			return BiarySearch(arr,target,0,pivot-1);
-		}
-		else{
-			return BiarySearch(arr,target,pivot+1,arr.length-1);
-		}
-	}
-	static int BiarySearch(int[] arr, int target,int start,int end){
-		while(start<=end){
-			int mid=start+(end-start)/2;
-			if(arr[mid]>target){
-				end=mid-1;
-			}
-			else if(arr[mid]<target){
-				start=mid+1;
-			}else{
-				return mid;
-			}
-		}
-		return -1;
-	}
-	static int findPivot(int[] arr){
-		int start=0;
-		int end=arr.length-1;
-		while(start<=end){
-			int mid=start + (end-start)/2;
 
-			//case 1: if middle element is greater than next element obviously it is pivot since it is sorted array.
-			if(mid<end && arr[mid]>arr[mid+1]){
-				return mid;
-			}
-			//case 2: if middle element is lesser than next element obviously it is pivot since it is sorted array.
-			if(mid>start && arr[mid]<arr[mid-1]){
-				return mid-1;
-			}
-			//case 3: if start is greater then or equall to middle then no need of searching next to mid element.
-			if(arr[start]>=arr[mid]){
-				end=mid-1;
-			}
-			//case 4: if start is less then or equall to middle then no need of searching before to mid element.
-			else{
-				start=mid+1;
-			}
-		}
-		return -1;
-	}
+public class RotatedBs {
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        int ans = RotatedTargetSearch(arr, target);
+        System.out.println(ans);
+    }
+
+    static int RotatedTargetSearch(int[] arr, int target) {
+        int pivot = findPivot(arr);
+        if (pivot == -1) { // this means the array is not rotated just do simple bs
+            return BiarySearch(arr, target, 0, arr.length - 1);
+        }
+        if (arr[pivot] == target) {
+            return pivot;
+        }
+
+        if (target >= arr[0]) {
+            return BiarySearch(arr, target, 0, pivot - 1);
+        } else {
+            return BiarySearch(arr, target, pivot + 1, arr.length - 1);
+        }
+    }
+
+    static int BiarySearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > target) {
+                end = mid - 1;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    static int findPivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            //case 1: if middle element is greater than next element obviously it is pivot since it is sorted array.
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            //case 2: if middle element is lesser than next element obviously it is pivot since it is sorted array.
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+            //case 3: if start is greater then or equall to middle then no need of searching next to mid element.
+            if (arr[start] >= arr[mid]) {
+                end = mid - 1;
+            } //case 4: if start is less then or equall to middle then no need of searching before to mid element.
+            else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
 }
