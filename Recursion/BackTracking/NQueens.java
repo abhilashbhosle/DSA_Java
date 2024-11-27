c2f48e7503399c2265fa207bb6990b5fdd61756f
Abhilash Bhosle <abhilashbhosle10@gmail.com>
2024-11-27 12:05:48 +0530

    N queens print count and array problem is resolved

--- Recursion/BackTracking/NQueens.java
+++ Recursion/BackTracking/NQueens.java
@@ -2,19 +2,19 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Practice {
+public class NQueens {
 
     public static void main(String[] args) {
         int n = 5;
         boolean[][] board = new boolean[n][n];
-        System.out.println(findPaths(board, n, 0));
+        System.out.println(findPaths(board, 0));
         List<List<String>> ans = findPathsArray(board, n, 0);
         System.out.println(ans);
     }
 
-    static int findPaths(boolean[][] board, int n, int row) {
+    static int findPaths(boolean[][] board, int row) {
         if (row == board.length) {
-            displayBoard(board);
+            printPossiblePath(board);
             System.out.println("");
             return 1;
         }
@@ -22,29 +22,41 @@ public class Practice {
         for (int col = 0; col < board.length; col++) {
             if (isSafe(board, row, col)) {
                 board[row][col] = true;
-                count += findPaths(board, n, row + 1);
+                count += findPaths(board, row + 1);
                 board[row][col] = false;
             }
         }
         return count;
     }
 
+    static void printPossiblePath(boolean[][] board) {
+        for (boolean[] i : board) {
+            for (boolean j : i) {
+                if (j) {
+                    System.out.print("Q ");
+                } else {
+                    System.out.print("X ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+
     static boolean isSafe(boolean[][] board, int row, int col) {
-        // check vertically
+
         for (int i = 0; i < row; i++) {
             if (board[i][col]) {
                 return false;
             }
         }
-        // check left diagonal
+        // left diagonal
         int maxLeft = Math.min(row, col);
         for (int i = 1; i <= maxLeft; i++) {
             if (board[row - i][col - i]) {
                 return false;
             }
         }
-        // check right diagonal
-        int maxRight = Math.min(row, board[0].length - col - 1);
+        int maxRight = Math.min(row, board.length - col - 1);
         for (int i = 1; i <= maxRight; i++) {
             if (board[row - i][col + i]) {
                 return false;
@@ -52,21 +64,8 @@ public class Practice {
         }
         return true;
     }
+    //==== Reaturning arraylist=====//
 
-    static void displayBoard(boolean[][] board) {
-        for (boolean[] i : board) {
-            for (boolean j : i) {
-                if (j) {
-                    System.out.print("Q ");
-                } else {
-                    System.out.print(".");
-                }
-            }
-            System.out.println("");
-        }
-    }
-
-//==== Reaturning arraylist=====//
     static List<String> displayBoardArray(boolean[][] board) {
         List<String> list = new ArrayList<>();
         for (boolean[] i : board) {
@@ -99,4 +98,5 @@ public class Practice {
         }
         return list;
     }
+
 }
