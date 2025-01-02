018389744776888edd4daec43c22fcb6efff7b7b
Abhilash Bhosle <abhilashbhosle10@gmail.com>
2025-01-02 16:03:20 +0530

    Maximum value in a generic tree

--- Trees/GenericTree/Generic.java
+++ Trees/GenericTree/Generic.java
@@ -3,24 +3,51 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 public class Generic {
-    private static  class Node {
+
+    private static class Node {
+
         int data;
         ArrayList<Node> children = new ArrayList<>();
     }
-	private static void display(Node node){
-		String str=node.data + "->";
-		for(Node child:node.children){
-			str+=child.data +", ";
-		}
-		str+=".";
-		System.out.println(str);
-		for(Node child:node.children){
-			display(child);
-		}
-	}
+
+    private static void display(Node node) {
+        String str = node.data + "->";
+        for (Node child : node.children) {
+            str += child.data + ", ";
+        }
+        str += ".";
+        System.out.println(str);
+        for (Node child : node.children) {
+            display(child);
+        }
+    }
+
+    private static int size(Node node) {
+        int s = 0;
+        for (Node child : node.children) {
+            int cs = size(child);
+            s = s + cs;
+        }
+        s = s + 1;
+        return s;
+    }
+
+    private static int maximum(Node node) {
+        int max = Integer.MIN_VALUE;
+        for (Node child : node.children) {
+            int cm = maximum(child);
+            if (cm > max) {
+                max = cm;
+            }
+        }
+        max = Math.max(node.data, max);
+        return max;
+    } 
+
+
     public static void main(String[] args) {
         int[] arr = {10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1};
-        Node root=null;
+        Node root = null;
         Stack<Node> st = new Stack<>();
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == -1) {
@@ -36,6 +63,8 @@ public class Generic {
                 st.push(t);
             }
         }
-		display(root);
+        display(root);
+        System.out.println(size(root));
+        System.out.println(maximum(root));
     }
 }
