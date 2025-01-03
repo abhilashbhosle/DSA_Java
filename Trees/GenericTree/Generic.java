32fb35bd6715f98a028fae686bcd83982f792a4d
Abhilash Bhosle <abhilashbhosle10@gmail.com>
2025-01-03 15:10:21 +0530

    Pre order and Post order Traversal

--- Trees/GenericTree/Generic.java
+++ Trees/GenericTree/Generic.java
@@ -32,15 +32,15 @@ public class Generic {
     }
 
     private static int maximum(Node node) {
-        int max = Integer.MIN_VALUE;
-        for (Node child : node.children) {
-            int cm = maximum(child);
-            if (cm > max) {
-                max = cm;
-            }
-        }
-        max = Math.max(node.data, max);
-        return max;
+      int max=Integer.MIN_VALUE;
+	  for(Node child:node.children){
+		int m=maximum(child);
+		if(m>max){
+			max=m;
+		}
+	  }
+	  max=Math.max(node.data, max);
+	  return max;
     } 
 
 	private static int height(Node node){
@@ -53,27 +53,40 @@ public class Generic {
 		return h;
 	}
 
+	private static void traversal(Node node){
+		System.out.println("Node Pre "+node.data);
+		for(Node child : node.children){
+			System.out.println("Edge Pre"+node.data+"--"+child.data);
+			traversal(child);
+			System.out.println("Edge Post"+node.data+"--"+child.data);
+		}
+		System.out.println("Node Post"+node.data);
+	}
+	
+
     public static void main(String[] args) {
         int[] arr = {10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1};
         Node root = null;
         Stack<Node> st = new Stack<>();
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == -1) {
-                st.pop();
-            } else {
-                Node t = new Node();
-                t.data = arr[i];
-                if (!st.isEmpty()) {
-                    st.peek().children.add(t);
-                } else {
-                    root = t;
-                }
-                st.push(t);
-            }
+           
+			if(arr[i]==-1){
+				st.pop();
+			}else{
+				Node t=new Node();
+				t.data=arr[i];
+				if(!st.isEmpty()){
+					st.peek().children.add(t);
+				}else{
+					root=t;
+				}
+				st.push(t);
+			}
         }
         display(root);
         System.out.println(size(root));
         System.out.println(maximum(root));
 		System.out.println(height(root));
+		traversal(root); 
     }
 }
